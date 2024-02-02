1b7d77af348128a337829a1a277e90958a6ad6ac
PeiMu <ds1231h@gmail.com>
2024-02-02 16:11:05 +0000

    change the default algorithm in query_split

--- dbgen/generate_query_split.sh
+++ dbgen/generate_query_split.sh
@@ -2,10 +2,12 @@ mkdir -p out/QuerySplit/
 
 cp out/queries/* out/QuerySplit/.
 
-split_algo="${1:-minsubquery}"
+split_algo="${1:-relationshipcenter}"
+order_decision="${1:-c_r}"
 
 for i in {1..22}; do
   sed -i "1s/^/switch to ${split_algo};\n/" out/QuerySplit/${i}.sql
+  sed -i "1s/^/switch to ${order_decision};\n/" out/QuerySplit/${i}.sql
 done
 
 
