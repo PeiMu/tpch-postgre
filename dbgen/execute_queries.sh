ea26115e966620db253ec580813abbf82a5be476
Pei Mu <ds1231h@gmail.com>
2024-02-28 11:56:59 +0000

    upload default explain result; update execute script

--- dbgen/execute_queries.sh
+++ dbgen/execute_queries.sh
@@ -4,6 +4,7 @@ iteration=10
 for i in {1.."${iteration}"}; do
   for sql in "${dir}"/*; do
     echo "execute ${sql}";
-    psql -f "${sql}" 2>&1|tee -a skinner_explained_tpch-3.txt;
+    #psql -f "${sql}" 2>&1|tee -a skinner_explained_tpch-3.txt;
+    psql -f "${sql}";
   done
 done
