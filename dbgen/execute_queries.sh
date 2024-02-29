4590296effea74231733a738f0efa42afab83b16
Pei Mu <ds1231h@gmail.com>
2024-02-29 09:55:49 +0000

    update script

--- dbgen/execute_queries.sh
+++ dbgen/execute_queries.sh
@@ -1,10 +1,15 @@
 dir="/home/pei/benchmarks/tpch-postgre/dbgen/out/skinner_explained"
 iteration=10
 
-for i in {1.."${iteration}"}; do
+rm -f result/*
+mkdir -p result/
+
+for i in $(eval echo {1.."${iteration}"}); do
   for sql in "${dir}"/*; do
-    echo "execute ${sql}";
-    #psql -f "${sql}" 2>&1|tee -a skinner_explained_tpch-3.txt;
-    psql -f "${sql}";
+    echo "execute ${sql}" 2>&1|tee -a skinner_explained_tpch-3_${i}.txt;
+    psql -f "${sql}" 2>&1|tee -a skinner_explained_tpch-3_${i}.txt;
+    #psql -f "${sql}";
   done
 done
+
+mv skinner_explained_tpch-3_* result/.
